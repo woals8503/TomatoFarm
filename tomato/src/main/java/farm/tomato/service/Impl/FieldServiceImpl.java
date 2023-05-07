4698e360834be8d7c487f36baece07dff85ebb44
woals8503 <woals6391@gmail.com>
2023-05-08 04:24:24 +0900

    커밋3

--- tomato/src/main/java/farm/tomato/service/Impl/FieldServiceImpl.java
+++ tomato/src/main/java/farm/tomato/service/Impl/FieldServiceImpl.java
@@ -54,6 +54,7 @@ public class FieldServiceImpl implements FieldService {
                         .tomatoes(tomatoes)
                         .fieldLength(o.getFieldLength())
                         .build());
+        boolean b = fieldDetailDTO.get().getFieldLength()[0][1];
         return fieldDetailDTO;
     }
 }
